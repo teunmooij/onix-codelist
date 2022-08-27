7247443ba554e65b7a5743a493585de22c7fe15e
teunmooij <tmooij@infinitaslearning.com>
2022-08-27 10:54:08 +0200

    basic test validation

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -1,15 +1,24 @@
+import fs from 'fs/promises';
 import { generateLists } from '../src/geneate-lists';
+
+jest.setTimeout(20000);
 
 jest.mock('fs/promises', () => {
   const realFs = jest.requireActual('fs/promises');
   return {
     ...realFs,
+    mkdir: jest.fn(),
     writeFile: jest.fn(),
   };
 });
 
 describe('e2e test', () => {
-  it('generates the fiels', async () => {
+  it('generates the files', async () => {
     await generateLists();
+
+    expect(fs.mkdir).toHaveBeenCalledTimes(1);
+    expect(fs.mkdir).toHaveBeenCalledWith('gen/lists', { recursive: true });
+
+    expect(fs.writeFile).toHaveBeenCalledTimes(162);
   });
 });
